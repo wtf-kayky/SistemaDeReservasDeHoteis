a02de757eec16f8b0448d2c1a1f7f1fd4a619a95
Kayke.Dev <kaykevieira.dev@gmail.com>
2026-09-16 15:38:09 -0300

    Update QuartoDeLuxo.java

--- src/SistemaDeReserva/QuartoDeLuxo.java
+++ src/SistemaDeReserva/QuartoDeLuxo.java
@@ -1,4 +1,19 @@
 package SistemaDeReserva;
+public class QuartoDeLuxo extends  Quarto {
 
-public class QuartoDeLuxo {
+    private double taxaDeServiço = 29.99;
+
+    public QuartoDeLuxo(int numero, double precoPorQuarto, boolean ocupado, double taxaDeServiço) {
+        super(numero, precoPorQuarto, ocupado);
+        this.taxaDeServiço = taxaDeServiço;
+    }
+
+    @Override
+    public double calcularValor(int quantidadesDeNoites) {
+
+        double valorTotal = (double) (quantidadesDeNoites * getPrecoPorNoite() + taxaDeServiço);
+
+        System.out.println("Valor a ser pago pelo quarto de luxo " + valorTotal);
+        return valorTotal;
+    }
 }
