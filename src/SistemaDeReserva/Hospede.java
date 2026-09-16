c0a0badb3be8634d27c6923db359522685f6aacc
Kayke.Dev <kaykevieira.dev@gmail.com>
2026-09-16 15:36:43 -0300

    Update Hospede.java

--- src/SistemaDeReserva/Hospede.java
+++ src/SistemaDeReserva/Hospede.java
@@ -1,4 +1,37 @@
 package SistemaDeReserva;
 
 public class Hospede {
+
+    private String nome;
+    private int idade;
+
+    public Hospede(String nome, int idade) {
+        if (idade < 0) {
+            throw new RuntimeException("Idade insuficiente");
+        } else {
+            this.idade = idade;
+            this.nome = nome;
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        if (idade <= 0) {
+            throw new RuntimeException("Idade isuficiente");
+        } else {
+            this.idade = idade;
+        }
+    }
 }
+
