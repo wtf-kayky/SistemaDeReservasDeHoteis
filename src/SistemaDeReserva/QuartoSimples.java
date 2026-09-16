d2adc9df103f82c050fa7b39412a2cfa0075d707
Kayke.Dev <kaykevieira.dev@gmail.com>
2026-09-16 15:38:28 -0300

    Update QuartoSimples.java

--- src/SistemaDeReserva/QuartoSimples.java
+++ src/SistemaDeReserva/QuartoSimples.java
@@ -1,4 +1,27 @@
 package SistemaDeReserva;
 
-public class QuartoSimples {
+public class QuartoSimples extends Quarto{
+
+    private String cama;
+
+    public QuartoSimples(int numero, double precoPorQuarto, boolean ocupado, String cama) {
+        super(numero, precoPorQuarto, ocupado);
+        this.cama = cama;
+    }
+
+    public String getCama() {
+        return cama;
+    }
+
+    public void setCama(String cama) {
+        this.cama = cama;
+    }
+
+    @Override
+    public double calcularValor(int quantidadesDeNoites) {
+        double valorTotal = (double) (quantidadesDeNoites * getPrecoPorNoite());
+        System.out.println("Valor a ser pago para hotel pelo quarto simples "+ valorTotal);
+
+        return valorTotal;
+    }
 }
